b58be279986e7b601a49b65921d89adcda145a96
Richard Bach <balutbomber@gmail.com>
2021-05-10 21:06:25 -0700

    fix finding metadata file when no extension (#28)
    * fix finding metadata file when no extension
    
    * fix lint issues

--- src/pipelines/components/s3_brokers.py
+++ src/pipelines/components/s3_brokers.py
@@ -15,7 +15,13 @@ filename_investigator = FilenameInvestigator()
 
 
 def corresponding_json_file_exists(bucket_name, non_json_filename, exists_check):
-    stripped_filename = filename_investigator.determine_base_filename(non_json_filename)
+    try:
+        stripped_filename = filename_investigator.determine_base_filename(non_json_filename)
+    except Exception:
+        root_directory = filename_investigator.determine_root_directory(non_json_filename)
+        non_json_filename_minus_root_directory = non_json_filename[len(root_directory) + 1:]
+        stripped_filename = filename_investigator.determine_root_directory(non_json_filename_minus_root_directory)
+
     json_filename = f'{stripped_filename}.json'
     logging.debug(f'Checking for existence of {bucket_name}/{json_filename}, corresponding to {non_json_filename}')
     exists = s3_service.file_exists(bucket_name, json_filename)
